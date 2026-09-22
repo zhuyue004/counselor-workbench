03376f61204bf88c2056ace371a119c29f2bd1d0
ZhuYue <zhuyue004@users.noreply.github.com>
2026-09-22 17:04:04 +0800

    chore: release v1.0.26

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'counselor-workbench-v1-20260922-leave';
+const CACHE = 'counselor-workbench-v1-20260922-leave-search';
 const ASSETS = ['./', './index.html', './leave.html', './leave.js', './styles.css', './app.js', './data.js', './cloudbase.js', './xlsx.js', './transfer.js', './vendor/fflate.js', './vendor/qrcode.js', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
